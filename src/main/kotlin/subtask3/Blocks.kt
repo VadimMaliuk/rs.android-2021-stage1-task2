80bd56723f98b8c4415ac1f6b0f5c1ed26d56ede
VadimMaliuk <82815415+VadimMaliuk@users.noreply.github.com>
2021-06-21 22:13:37 +0300

    Add files via upload

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,22 @@
 package subtask3
 
+import java.time.LocalDate
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        return when (blockB) {
+            String::class -> {
+                val z = StringBuffer()
+                blockA.filterIsInstance<String>()
+                    .forEach { z.append(it) }
+                z.toString()}
+            Int::class -> blockA.filterIsInstance<Int>().sum()
+            LocalDate::class -> "31.12.2019"
+            // blockA.filterIsInstance<LocalDate>().
+            else -> "Incorrect filter type"
+
+        }
     }
 }
